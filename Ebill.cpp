c5d4691c3ab632446e8f598fa508e6fee9170247
Aarya <165459210+Aarya-Musale@users.noreply.github.com>
2024-05-13 12:36:31 +0530

    Add files via upload

---
+++ Ebill.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class Bill
+{
+private:
+int unit;
+double total bill;
+public:
+char customer name;
+int fixed value
+void Bill()
+{
+fixed value=120;
+}
+void Bill(int a,int b,int c)
+{
+1 unit=8
+total bill=fixed value*unit used
+}
+};
+int main()
+{
+void getdata()
+{
+std::cout<<"enter name of customer=\n";
+cin<<b;
+std::cout<<"1 unit=8Rs/-\n"<<a;
+std::cout<<"total bill="<<c;
+Bill.b1;
+Bill.b2;
+b2.getdata();
+return 0;
+}
+}
+
+
+
+
